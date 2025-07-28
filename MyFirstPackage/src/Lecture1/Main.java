ebbb9427290aff13a6e5c43c6f13b2c641b8f398
victorstephanovbg-debug <victor.stephanov.bg@gmail.com>
2025-07-28 10:27:41 +0300

    new code

--- MyFirstPackage/src/Lecture1/Main.java
+++ MyFirstPackage/src/Lecture1/Main.java
@@ -1,11 +1,17 @@
 package Lecture1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String FirstName = "Viktor";
-        String MiddleName = "Dimitrov";
-        String Surname = "Stefanov";
-        System.out.println(FirstName + " " + MiddleName + " " + Surname);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your first name:");
+        String FirstName = scanner.nextLine();
+        System.out.println("Enter your middle name:");
+        String MiddleName = scanner.nextLine();
+        System.out.println("Enter your surname:");
+        String Surname = scanner.nextLine();
+        System.out.println("Your full name is: " + FirstName + " " + MiddleName + " " + Surname);
         System.out.println("-----------");
         System.out.print("\n");
         System.out.println("   *");
