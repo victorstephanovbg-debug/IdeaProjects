1f8886ac054526416e4ddaa4c4b9293e4094902c
Viktor Stefanov <victor.stephanov.bg@gmail.com>
2025-08-09 23:33:58 +0300

    Update AreaTriangle.java

--- MyFirstPackage/src/Lecture1/AreaTriangle.java
+++ MyFirstPackage/src/Lecture1/AreaTriangle.java
@@ -6,13 +6,13 @@ public class area_triangle {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the a-side of the triangle:");
-        float a_side = scanner.nextFloat();
+        float aSide = scanner.nextFloat();
         System.out.println("Enter the b-side of the triangle:");
-        float b_side = scanner.nextFloat();
+        float bSide = scanner.nextFloat();
         System.out.println("Enter the c-side of the triangle:");
-        float c_side = scanner.nextFloat();
-        float perimeter = a_side + b_side + c_side;
-        double area = Math.sqrt((perimeter/2)*(perimeter/2 - a_side)*(perimeter/2 - b_side)*(perimeter/2 - c_side));
+        float cSide = scanner.nextFloat();
+        float perimeter = aSide + bSide + cSide;
+        double area = Math.sqrt((perimeter/2)*(perimeter/2 - aSide)*(perimeter/2 - bSide)*(perimeter/2 - cSide));
         System.out.println("The area of the triangle is: " + area);
     }
 }
