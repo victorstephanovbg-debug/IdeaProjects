d5f48220d8609c1b02e68954edfa29383678e447
Viktor Stefanov <victor.stephanov.bg@gmail.com>
2025-08-09 23:28:35 +0300

    Update and rename convertor_minutes_days_years.java to ConvertorMinutesDaysYears.java

--- MyFirstPackage/src/Lecture1/ConvertorMinutesDaysYears.java
+++ MyFirstPackage/src/Lecture1/ConvertorMinutesDaysYears.java
@@ -9,7 +9,7 @@ public class convertor_minutes_days_years {
         int minutes = scanner.nextInt();
         float days = (float) minutes / (60*24);
         System.out.println(minutes + " minutes = " + days + " days.");
-        float years = (float) minutes / (60*24*365);
+        float years = days/365;
         System.out.println(minutes + " minutes = " + years + " years.");
     }
 }
