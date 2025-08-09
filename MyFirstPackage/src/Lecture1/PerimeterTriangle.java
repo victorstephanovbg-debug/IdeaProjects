58dfc6aeb2a6c62fe026857f8fcdd447731681a7
Viktor Stefanov <victor.stephanov.bg@gmail.com>
2025-08-09 23:37:16 +0300

    Update and rename perimeter_triangle.java to PerimeterTriangle.java

--- MyFirstPackage/src/Lecture1/PerimeterTriangle.java
+++ MyFirstPackage/src/Lecture1/PerimeterTriangle.java
@@ -6,12 +6,12 @@ public class perimeter_triangle {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the a-side of the triangle:");
-        float a_side = scanner.nextFloat();
+        float aSide = scanner.nextFloat();
         System.out.println("Enter the b-side of the triangle:");
-        float b_side = scanner.nextFloat();
+        float bSide = scanner.nextFloat();
         System.out.println("Enter the c-side of the triangle:");
-        float c_side = scanner.nextFloat();
-        float perimeter = a_side + b_side + c_side;
+        float cSide = scanner.nextFloat();
+        float perimeter = aSide + bSide + cSide;
         System.out.println("The perimeter of the triangle is: " + perimeter);
     }
 }
